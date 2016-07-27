ef4c3a838d0b8fbfaa2b6097f267453107513635
unknown <ak46860@logti-a3324-34.ens.ad.etsmtl.ca>
2016-07-27 17:58:07 -0400

    Fix classification

--- src/application/Classification.java
+++ src/application/Classification.java
@@ -6,10 +6,14 @@ import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 
+import libsvm.svm;
+import libsvm.svm_model;
 import weka.classifiers.Classifier;
-import weka.core.Instance;
+import weka.core.Attribute;
 import weka.core.Instances;
+import weka.core.WekaPackageManager;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Normalize;
 import weka.filters.unsupervised.attribute.Remove;
@@ -24,10 +28,10 @@ public class Classification {
 	private String inputFilePath2;
 	private String outputPath;
 	
-	private static String[] typeMusic = {"BIG_BAND,BLUES_CONTEMPORARY","COUNTRY_TRADITIONAL","DANCE","ELECTRONICA","EXPERIMENTAL","FOLK_INTERNATIONAL"
-			                     ,"GOSPEL","GRUNGE_EMO","HIP_HOP_RAP","JAZZ_CLASSIC","METAL_ALTERNATIVE","METAL_DEATH","METAL_HEAVY","POP_CONTEMPORARY"
-			                     ,"POP_INDIE","POP_LATIN","PUNK,REGGAE","RNB_SOUL","ROCK_ALTERNATIVE","ROCK_COLLEGE","ROCK_CONTEMPORARY","ROCK_HARD"
-			                     ,"ROCK_NEO_PSYCHEDELIA"};
+	private static String[] typeMusic = {"BIG_BAND", "BLUES_CONTEMPORARY", "COUNTRY_TRADITIONAL", "DANCE", "ELECTRONICA", 
+			"EXPERIMENTAL", "FOLK_INTERNATIONAL", "GOSPEL", "GRUNGE_EMO", "HIP_HOP_RAP", "JAZZ_CLASSIC", "METAL_ALTERNATIVE", 
+			"METAL_DEATH", "METAL_HEAVY", "POP_CONTEMPORARY", "POP_INDIE", "POP_LATIN", "PUNK", "REGGAE", "RNB_SOUL", 
+			"ROCK_ALTERNATIVE", "ROCK_COLLEGE", "ROCK_CONTEMPORARY", "ROCK_HARD", "ROCK_NEO_PSYCHEDELIA"};
 	
 	Classification(String inputFilePath1, String inputFilePath2, String outputPath) {
 		this.inputFilePath1 = inputFilePath1;
@@ -48,29 +52,28 @@ public class Classification {
 	
 	private static String classToNominal(int cl)
 	{
-		return typeMusic[cl-1];
+		return typeMusic[cl];
 	}
 	
-	private void preTraitement(String inputFilePath, Instances toClassify) throws Exception
-	{
-		
+	private Instances prepareData(String inputFilePath, Instances toClassify) throws Exception {
 		Normalize normalize = new Normalize();
 		String[] option = {"-S", "1.0",  "-T", "0.0"};
 		normalize.setOptions(option);
 		normalize.setInputFormat(toClassify);
-		Filter.useFilter(toClassify, normalize);
-		
+		Filter.useFilter(toClassify,normalize);
+
+
 		Remove rmTag = new Remove();
 		String[] rmOptions = {"-R", "1,2"};
 		rmTag.setOptions(rmOptions);
 		rmTag.setInputFormat(toClassify);
 		toClassify = Filter.useFilter(toClassify, rmTag);
-		
+
 		Remove rmUseless = new Remove();
 		String[] options = new String[2];
-		
+
 		if(inputFilePath.contains("mfcc")){
-		
+
 			options[0] = "-R";
 
 			rmUseless = new Remove();
@@ -78,29 +81,34 @@ public class Classification {
 			{
 				if (!mccListValue.contains(i+1))
 				{
-					if(options[1] == null){
+					if(options[1] == null)
+					{
 						options[1] = Integer.toString(i+1);
-					} else {
+					} 
+					else 
+					{
 						options[1] = options[1] + "," + Integer.toString(i+1);
 					}
 				}
 			}
-			
+
 		}
-		else if (inputFilePath.contains("ssd")) {
-		
-			
+		else if (inputFilePath.contains("ssd")) 
+		{
+
 			options[0] = "-R";
-	
+
 			rmUseless = new Remove();
 			for (Integer i = 0; i < 168; i++)
 			{
-				
+
 				if (!ssdListValue.contains(i+1))
 				{
 					if(options[1] == null){
 						options[1] = Integer.toString(i+1);
-					} else {
+					} 
+					else 
+					{
 						options[1] = options[1] + "," + Integer.toString(i+1);
 					}
 				}
@@ -108,27 +116,31 @@ public class Classification {
 		} 
 		else if (inputFilePath.contains("deriv")) 
 		{
+
 			options[0] = "-R";
-		
+
 			rmUseless = new Remove();
 			for (Integer i = 0; i < 96; i++)
 			{
-				
+
 				if (!derivateListValue.contains(i+1))
 				{
-					if(options[1] == null){
+					if(options[1] == null)
+					{
 						options[1] = Integer.toString(i+1);
-					} else {
+					} 
+					else 
+					{
 						options[1] = options[1] + "," + Integer.toString(i+1);
 					}
 				}
 			}
+
 		}
-		
-		
 		rmUseless.setOptions(options);
 		rmUseless.setInputFormat(toClassify);
 		toClassify = Filter.useFilter(toClassify, rmUseless);
+		return toClassify;
 	}
 	
 	public  void startClassification() throws Exception{
@@ -144,15 +156,24 @@ public class Classification {
 		ObjectInputStream j48InputStream = new ObjectInputStream(getClass().getResourceAsStream("/models/treessd.model"));
 		Classifier j48Classifier = (Classifier) j48InputStream.readObject();
 		
-		ObjectInputStream knnInputStream = new ObjectInputStream(getClass().getResourceAsStream("/models/knn.model"));
+		/*WekaPackageManager.loadPackages( false, true, false );
+		ObjectInputStream svmInputStream = new ObjectInputStream(getClass().getResourceAsStream("/models/svmderivatives.model"));
+		Classifier svmClassifier = (Classifier) svmInputStream.readObject();*/
+		//LibSVM svm = (LibSVM) weka.core.SerializationHelper.read("/models/treessd.model");
+		
+		
+		ObjectInputStream knnInputStream = new ObjectInputStream(getClass().getResourceAsStream("/models/model.model"));
 		Classifier knnClassifier = (Classifier) knnInputStream.readObject();
-
-		preTraitement(inputFilePath1, toClassify1);
-		preTraitement(inputFilePath2, toClassify2);
+		
+		System.out.println("Filtering data...");
+		toClassify1 = prepareData(inputFilePath1, toClassify1);
+		toClassify2 = prepareData(inputFilePath2, toClassify2);
 		
 		j48InputStream.close();
 		knnInputStream.close();
+		//svmInputStream.close();
 
+		System.out.println("Classifying data...");
 		classifyDataSet(toClassify1, toClassify2, j48Classifier, knnClassifier, outputPath);
 	}
 	
@@ -163,27 +184,35 @@ public class Classification {
 		
 		//Classify data according to the classifier passed to the method.
 		for(int i = 0; i < toClassify1.numInstances(); i++){
+			
 			double[] classifier1 = classifierToUse1.distributionForInstance(toClassify1.get(i));
 			double[] classifier2 = classifierToUse2.distributionForInstance(toClassify2.get(i));
 			double sumTemp = 0;
 			int indexMajority = 0;
-			
+			double bestPercent = 0;
+			int bestIndex = 0;
+			//System.out.println(classifierToUse1.classifyInstance(toClassify1.get(i)) + " : " + classifierToUse2.classifyInstance(toClassify2.get(i)));
 			for(int j = 0; j < classifier1.length; j++)
 			{
-				double sum = classifier1[j] * 2 + classifier2[j];
+				//double sum = classifier1[j] + classifier2[j];
 				
-				if (sum > sumTemp)
+				/*if (sum > sumTemp)
 				{
 					sumTemp = sum;
 					indexMajority = j;
+				}*/
+				if(classifier1[j] > bestPercent){
+					bestPercent = classifier1[j];
+					bestIndex = j;
+				} else if (classifier2[j] > bestPercent){
+					bestPercent = classifier2[j];
+					bestIndex = j;
 				}
 				
+				
 			}
-			
-			output.println(classToNominal(indexMajority));
+			output.println(classToNominal(bestIndex));
 		}
-		
 		output.close();
-		
 	}
 }
